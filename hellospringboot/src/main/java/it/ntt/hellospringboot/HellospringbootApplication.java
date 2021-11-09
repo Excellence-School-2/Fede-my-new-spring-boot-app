6b2de606acbf4001a6c398c7b75f9720eda62599
Federica Bongermino <federica.bonger@gmail.com>
2021-11-09 16:34:25 +0100

    aggiorna

--- hellospringboot/src/main/java/it/ntt/hellospringboot/HellospringbootApplication.java
+++ hellospringboot/src/main/java/it/ntt/hellospringboot/HellospringbootApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HellospringbootApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("ciao a tutti");
 		SpringApplication.run(HellospringbootApplication.class, args);
 	}
 
